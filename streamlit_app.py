398f44157f56d26dc79885e57e8584d776795ef8
Engineer-Bilal-Channa <engineer.bilal.channa@gmail.com>
2025-02-26 11:24:36 +0000

    upload file done

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,21 @@ import time
 
 st.title("Dashboard App ️‍🔥")
 
-df = pd.read_csv("/workspaces/blank-app/data/4g-belts-dtot.csv")
+# df = pd.read_csv("/workspaces/blank-app/data/4g-belts-dtot.csv")
+
+# upload file
+
+# File uploader widget
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+# Check if a file has been uploaded
+if uploaded_file is not None:
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(uploaded_file)
+    
+    # Show the first 5 rows of the CSV
+    st.write("Preview of the CSV file:")
+    st.dataframe(df)
 
 ##
 ### clean data
@@ -36,6 +50,3 @@ selected_KPI = st.selectbox(
 df_plot = df_updated[['date', selected_KPI]]
 st.write(f'{selected_KPI} vs {selected_belt}')
 st.line_chart(df_plot.set_index("date"))
-
-
-
